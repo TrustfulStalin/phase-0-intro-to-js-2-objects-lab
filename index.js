14cc091f44a2ce0b8c646832e4eedf8ffb91f5f6
Tevon Talbert <TevonTalbert@hotmail.com>
2023-08-24 01:35:01 -0500

    adding work

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
 // Write your solution in this file!
+
+const employee = {
+  name: "Joe",
+  streetAddress: "1st street"
+  
+  
+};
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+
+
+  const obj = { ...employee };
+
+  
+  return {
+    ...obj,
+    [key]: value,
+  }
+};
+
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value ) {
+
+  employee[ key ] = value;
+  
+
+
+
+  return employee;
+}
+
+
+
+function deleteFromEmployeeByKey(employee, key) {
+
+  const obj = { ...employee };
+
+  delete obj.key;
+
+
+
+  return {
+    ...obj.key
+  }
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+
+  const obj = { ...employee };
+  employee[key] = obj.key;
+  delete employee.key;
+  
+
+  
+
+  console.log(obj.key);
+  console.log(employee.key);
+
+  return employee;
+}
+
